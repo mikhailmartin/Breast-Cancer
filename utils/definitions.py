0bedd61b3b803e6e259ab76cb1a0eee7347e16d1
MikhailShevchenko <bringmetohappiness@mail.ru>
2022-02-21 00:46:51 +0300

    переосмыслил структуру ETL + ренейминг

--- utils/definitions.py
+++ utils/definitions.py
@@ -241,14 +241,13 @@ NORM_PATH = os.path.join('data', 'origin', 'Таблица учёта НОРМА
 BENIGN_TUMOR_PATH = os.path.join('data', 'origin', 'Таблица учёта ДОБРОКАЧЕСТВЕННАЯ.xlsx')
 MALIGNANT_TUMOR_PATH = os.path.join('data', 'origin', 'Таблица учёта ОНКОЛОГИЯ.xlsx')
 # данные после ETL
-ETLED_DATA_PATH_step1 = os.path.join('data', 'ETLed data step1.xlsx')
-ETLED_DATA_PATH_my_tree = os.path.join('data', 'ETLed data my tree.xlsx')
-ETLED_DATA_PATH_nn = os.path.join('data', 'ETLed data nn.xlsx')
-
+ETLED_DATA_PATH_step_1 = os.path.join('data', 'ETLed data step 1.xlsx')
+ETLED_DATA_PATH_step_2 = os.path.join('data', 'ETLed data step 2.xlsx')
+# train/val/test разбивка в CSV-файлах
 ENTIRE_DATA_PATH = os.path.join('data', 'train val test', 'entire data.csv')
 TRAIN_DATA_PATH = os.path.join('data', 'train val test', 'train data.csv')
 VAL_DATA_PATH = os.path.join('data', 'train val test', 'val data.csv')
 TEST_DATA_PATH = os.path.join('data', 'train val test', 'test data.csv')
-
+# обученные модели
 GENERALIZING_MODEL = os.path.join(os.getcwd(), 'models', 'generalizing model')
 OVERFITTED_MODEL = os.path.join(os.getcwd(), 'models', 'overfitted model')
