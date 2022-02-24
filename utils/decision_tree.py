6d1bb98292d93cb6e5f38434c841d66e0118f7d5
MikhailShevchenko <bringmetohappiness@mail.ru>
2022-02-24 10:08:09 +0300

    апдейт дерева решений

--- utils/decision_tree.py
+++ utils/decision_tree.py
@@ -7,7 +7,7 @@ import math
 from graphviz import Digraph
 
 
-def _counter(function):
+def counter(function):
     """Декоратор-счётчик."""
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
@@ -19,10 +19,27 @@ def _counter(function):
 
 class DecisionTree:
     """Дерево решений."""
-    def __init__(self, *, min_samples_split=2, min_samples_leaf=1, min_impurity_decrease=0.05):
+    def __init__(
+            self,
+            *,
+            criterion='entropy',
+            min_samples_split=2,
+            min_samples_leaf=1,
+            min_impurity_decrease=0.05
+    ):
+        if criterion != 'entropy':
+            print('Я пока не умею работать с таким критерием. Посчитаю через энтропию.')
+        self._criterion = 'entropy'
         self._min_samples_split = min_samples_split
         self._min_samples_leaf = min_samples_leaf
         self._min_impurity_decrease = min_impurity_decrease
+
+        self.feature_names = None
+        self.class_names = None
+        self.categorical_feature_names = None
+        self.numerical_feature_names = None
+        self.tree = None
+        self.graph = None
 
     def fit(self, X, Y, categorical_feature_names, numerical_feature_names, *, special_cases=None):
         """Обучает дерево решений.
@@ -52,7 +69,7 @@ class DecisionTree:
 
         self.tree = self._generate_node(X, Y, None, available_feature_names, special_cases)
 
-    @_counter
+    @counter
     def _generate_node(self, X, Y, feature_value, available_feature_names, special_cases=None):
         """Рекурсивная функция создания узлов дерева.
 
@@ -68,11 +85,14 @@ class DecisionTree:
             node: узел дерева.
         """
         available_feature_names = available_feature_names.copy()
+
         # выбор лучшего признака для разбиения
-        best_gain = self._min_impurity_decrease
+        impurity = self._impurity(Y)
+
         best_feature = None
+        best_gain = self._min_impurity_decrease
         for feature_name in available_feature_names:
-            current_gain = self._information_gain(X, Y, feature_name)
+            current_gain = self._information_gain(X, Y, feature_name, impurity)
             if isinstance(best_gain, float) and isinstance(current_gain, float):
                 if best_gain < current_gain:
                     best_gain = current_gain
@@ -129,13 +149,113 @@ class DecisionTree:
 
         return node
 
-    def _split(self, X, Y, feature_name, threshold):
-        """Расщепляет множество согласно признаку.
+    def _impurity(self, Y):
+        """Считает загрязнённость для множества.
+
+        Args:
+            Y (pd.Series): c метками для множества.
+
+        Returns:
+            impurity: загрязнённость множества.
+        """
+        impurity = None
+        if self._criterion == 'entropy':
+            impurity = self._entropy(Y)
+
+        return impurity
+
+    @staticmethod
+    def _entropy(Y):
+        """Считает энтропию в множестве.
+
+        Args:
+            Y (pd.Series): c метками для множества.
+
+        Returns:
+            entropy: энтропия множества.
+        """
+        n = Y.shape[0]  # количество точек в множестве
+
+        entropy = 0
+        for label in set(Y.tolist()):  # перебор по классам
+            m_i = (Y == label).sum()
+            if m_i != 0:
+                entropy -= (m_i / n) * math.log2(m_i / n)
+
+        return entropy
+
+    def _information_gain(self, X, Y, feature_name, impurity):
+        """Возвращает прирост информативности для разделения по признаку.
+
+        Формула в LaTeX:
+        Gain(A, Q) = H(A, S) -\sum\limits^q_{i=1} \frac{|A_i|}{|A|} H(A_i, S),
+        где
+        A - множество точек данных;
+        Q - метка данных;
+        q - множество значений метки, т.е. классы;
+        S - признак;
+        A_i - множество элементов A, на которых Q имеет значение i.
 
         Args:
             X: DataFrame с точками данных.
             Y: Series с соответствующими метками.
-            feature_name: признак, по которому будет происходить расщепление.
+            feature_name: название признака, по которому будет проходить разделение.
+            impurity: загрязнённость до разделения множества по признаку.
+
+        Returns:
+            information_gain: прирост информативности.
+        """
+        information_gain = None
+        if feature_name in self.categorical_feature_names:
+            information_gain = self._categorical_information_gain(X, Y, feature_name, impurity)
+        elif feature_name in self.numerical_feature_names:
+            information_gain = self._numerical_information_gain(X, Y, feature_name, impurity)
+
+        return information_gain
+
+    def _categorical_information_gain(self, X, Y, feature_name, impurity):
+        """Считает прирост информации для разделения по категориальному признаку."""
+        A = X.shape[0]
+        second_term = 0
+        for feature_value in set(X[feature_name].tolist()):
+            A_i = (X[feature_name] == feature_value).sum()
+            y_i = Y[X[feature_name] == feature_value]
+            second_term += (A_i/A) * self._impurity(y_i)
+
+        categorical_information_gain = impurity - second_term
+
+        return categorical_information_gain
+
+    def _numerical_information_gain(self, X, Y, feature_name, impurity):
+        """Считает прирост информации для разделения по численному признаку."""
+        A = X.shape[0]
+
+        best_gain = 0
+        best_threshold = None
+        for threshold in range(int(X[feature_name].max())):
+            A_less = (X[feature_name] < threshold).sum()
+            y_less = Y[X[feature_name] < threshold]
+            A_more = (X[feature_name] >= threshold).sum()
+            y_more = Y[X[feature_name] >= threshold]
+
+            numerical_information_gain = (
+                    impurity -
+                    (A_less/A) * self._impurity(y_less) -
+                    (A_more/A) * self._impurity(y_more))
+
+            if best_gain is None or best_gain < numerical_information_gain:
+                best_gain = numerical_information_gain
+                best_threshold = threshold
+
+        return best_gain, best_threshold
+
+    def _split(self, X, Y, feature_name, threshold):
+        """Разделяет множество по признаку.
+
+        Args:
+            X: DataFrame с точками данных.
+            Y: Series с соответствующими метками.
+            feature_name: признак, по которому будет происходить разделение.
             threshold: порог разбиения для численного признака.
 
         Returns:
@@ -176,119 +296,6 @@ class DecisionTree:
 
         return xs, ys, feature_values
 
-    def _information_gain(self, X, Y, feature_name):
-        """Считает прирост информации для разделения по признаку.
-
-        Формула в LaTeX:
-        Gain(A, Q) = H(A, S) -\sum\limits^q_{i=1} \frac{|A_i|}{|A|} H(A_i, S),
-        где
-        A - множество точек данных;
-        Q - метка данных;
-        q - множество значений метки, т.е. классы;
-        S - признак;
-        A_i - множество элементов A, на которых Q имеет значение i.
-        """
-        if X[feature_name].isnull().any():
-            print('Входное множество содержит NaN')
-            print(f'Признак - {feature_name}')
-
-        gain = None
-        if feature_name in self.categorical_feature_names:
-            gain = self._categorical_information_gain(X, Y, feature_name)
-        elif feature_name in self.numerical_feature_names:
-            gain = self._numerical_information_gain(X, Y, feature_name)
-
-        return gain
-
-    def _categorical_information_gain(self, X, Y, feature_name):
-        """Считает прирост информации для разделения по категориальному признаку."""
-        a = self._categorical_feature_entropy(X, feature_name)
-
-        b = 0
-        for class_name in self.class_names:
-            A_i = (Y == class_name).sum()
-            A = X.shape[0]
-            x = X[Y == class_name]
-            b += (A_i/A) * self._categorical_feature_entropy(x, feature_name)
-
-        gain = a - b
-
-        return gain
-
-    def _numerical_information_gain(self, X, Y, feature_name):
-        """Считает прирост информации для разделения по численному признаку."""
-        best_gain = 0
-        best_threshold = None
-        for threshold in range(int(X[feature_name].max())):
-            a = self._numerical_feature_entropy(X, feature_name, threshold)
-
-            b = 0
-            for class_name in self.class_names:
-                A_i = (Y == class_name).sum()
-                A = X.shape[0]
-                x = X[Y == class_name]
-                b += (A_i / A) * self._numerical_feature_entropy(x, feature_name, threshold)
-
-            gain = a - b
-
-            if best_gain is None or best_gain < gain:
-                best_gain = gain
-                best_threshold = threshold
-
-        return best_gain, best_threshold
-
-    @staticmethod
-    def _categorical_feature_entropy(X, categorical_feature_name):
-        """Считает энтропию для категориального признака.
-
-        Формула в LaTeX:
-        H(A, S) = -\sum\limits^s_{i = 1} \frac{m_i}{n} \log_2 \frac{m_i}{n},
-        где
-        A - множество точек данных;
-        n - количество точек данных в A;
-        S - признак;
-        s - множество значений признака S;
-        m_i - количество точек данных, имеющих значение s признака S;
-        """
-        n = X.shape[0]  # количество точек данных в обучающем наборе
-
-        entropy = 0
-        # перебор по значениям признака
-        for feature_value in set(X[categorical_feature_name].tolist()):
-            m_i = (X[categorical_feature_name] == feature_value).sum()
-            if m_i != 0:
-                entropy -= (m_i/n) * math.log2(m_i/n)
-
-        return entropy
-
-    @staticmethod
-    def _numerical_feature_entropy(X, numerical_feature_name, threshold):
-        """Считает энтропию для численного признака.
-
-        Формула в LaTeX:
-        H(A, S) = -\frac{m}{n} \log_2 \frac{m}{n} - \frac{n - m}{n} \log_2 \frac{n - m}{n}
-        где
-        A - множество точек данных;
-        n - количество точек данных в A;
-        S - признак;
-        m - количество точек данных, которые больше некоторого порога.
-        """
-        n = X.shape[0]  # количество точек данных в обучающем наборе
-
-        less = (X[numerical_feature_name] < threshold).sum()
-        more = (X[numerical_feature_name] >= threshold).sum()
-
-        if less == 0 and more == 0:
-            entropy = 0
-        elif less == 0:
-            entropy = -(more/n) * math.log2(more/n)
-        elif more == 0:
-            entropy = -(less/n) * math.log2(less/n)
-        else:
-            entropy = -(less/n) * math.log2(less/n) - (more/n) * math.log2(more/n)
-
-        return entropy
-
     def render(
             self,
             *,
@@ -311,10 +318,11 @@ class DecisionTree:
             graph: объект класса Digraph, содержащий описание графовой структуры дерева для
               визуализации.
         """
-        if not hasattr(self, 'graph'):
+        if self.graph is None:
             self._create_graph(rounded, show_num_samples, show_distribution, show_label)
         if kwargs:
             self.graph.render(**kwargs)
+
         return self.graph
 
     def _create_graph(self, rounded, show_num_samples, show_distribution, show_label):
@@ -326,7 +334,7 @@ class DecisionTree:
         self.graph = Digraph(name='дерево решений', node_attr=node_attr)
         self._add_node(self.tree, None, show_num_samples, show_distribution, show_label)
 
-    @_counter
+    @counter
     def _add_node(self, node, parent_name, show_num_samples, show_distribution, show_label):
         """Рекурсивно добавляет описание узла и его связь с родительским узлом (если имеется)."""
         node_name = f'node{self._add_node.count}'
